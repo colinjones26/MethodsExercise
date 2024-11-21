304ce3b437c0f2d5b649f322c2bfa9e6dd1b6a86
colinjones26 <colinanthonyjones1996@gmail.com>
2024-11-21 15:53:32 -0500

    Completed C# Story Program

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 namespace MethodsExercise
@@ -8,9 +11,10 @@ namespace MethodsExercise
     {
         static void Main(string[] args)
         {
-            //Promt the user for their name
+            //Prompt the user to tell their name
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine(); // input
+            Console.WriteLine($"My name is {name}");
 
             // string interpolation
             string favoriteColor = ("Blue");
@@ -19,17 +23,84 @@ namespace MethodsExercise
             //Prompt user for their favorite animal
             Console.WriteLine("What is your favorite animal?");
             string animal = Console.ReadLine(); // input
+            Console.WriteLine($"My favorite animal is {animal}");
 
             //Prompt user for their favorite vacation destination
             Console.WriteLine("Where would you like to vacation?");
             string vacation = Console.ReadLine(); // input
+            Console.WriteLine($"My favorite vacation destination is {vacation}");
 
-            //Promy user to explain why they chose that destination
+            //Promt user to explain why they chose that destination
             Console.WriteLine("What do you like about this destination?");
             string destination = Console.ReadLine(); // input
+            Console.WriteLine($"What I like about this destination is {destination}");
 
 
+
+            // Addition
+            Console.WriteLine("What is the sum of two numbers?");
+            int add = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose second number");
+            int add2 = int.Parse(Console.ReadLine());
+
+            var sum = Add(add, add2);
+
+            Console.WriteLine(sum);
+
+            //Subtraction
+            Console.WriteLine("What is the difference of two number?");
+            int sub = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose second number");
+            int sub2 = int.Parse(Console.ReadLine());
+
+            var diff = Diff(sub, sub2);
+
+            Console.WriteLine(diff);
+
+            //Multiplication
+            Console.WriteLine("What is the product of two number?");
+            int times = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose second number");
+            int times2 = int.Parse(Console.ReadLine());
+
+            var result = times * times2;
+
+            Console.WriteLine(result);
+
+            //Division
+            Console.WriteLine("What do you get when you divide two numbers?");
+            int div = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose second number");
+            int div2 = int.Parse(Console.ReadLine());
+
+            var quotient = div / div2;  
+
+            Console.WriteLine(quotient);
+        }
+
+        public static int Add(int numOne, int numTwo)
+         {
+            return numOne + numTwo;
+
+         }
+
+        public static int Diff(int numOne, int numTwo) 
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Times(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Qoutient(int numOne, int numTwo)                   
+        { 
+            return numOne / numTwo;
         }
     }
 }
-
--- MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472d02f9d75aac5a20b813b93fd9e5787f6c5186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d3bb89e65ec6f71b086157f2ce71c5286fe90c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
