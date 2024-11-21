b4fa126e628292f4b4f980d6870356b31fa028fa
colinjones26 <colinanthonyjones1996@gmail.com>
2024-11-21 12:37:21 -0500

    Completed C# Story Program

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Promt the user for their name
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine(); // input
+
+            // string interpolation
+            string favoriteColor = ("Blue");
+            Console.WriteLine($"My favortite color is {favoriteColor}");
+
+            //Prompt user for their favorite animal
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine(); // input
+
+            //Prompt user for their favorite vacation destination
+            Console.WriteLine("Where would you like to vacation?");
+            string vacation = Console.ReadLine(); // input
+
+            //Promy user to explain why they chose that destination
+            Console.WriteLine("What do you like about this destination?");
+            string destination = Console.ReadLine(); // input
+
+
         }
     }
 }
+
